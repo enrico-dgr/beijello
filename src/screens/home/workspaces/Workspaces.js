046f6205e458e32fba0cd4c85b007e679b5f65bc
enrico-dgr <elcidwar@gmail.com>
2022-01-31 11:16:44 +0100

    redux linked to components

--- src/screens/home/workspaces/Workspaces.js
+++ src/screens/home/workspaces/Workspaces.js
@@ -1,82 +1,76 @@
+import "./Workspaces.css";
 
 import React, { Component } from "react";
-import "./Workspaces.css"
-/* redux */
-import { connect } from "react-redux";
-
-
-import React, { Component } from "react";
-/* Services WorkspaceApi */
-
-import { addWorkSpace, getWorkSpacesByEmail, setWorkSpaces } from "../../../services/workspaceApi";
-
+import {
+	addWorkSpace,
+	getWorkSpacesByEmail,
+} from "../../../services/workspaceApi";
 
 /* funcComponents */
 import Button from "../../../components/funcComponents/UI/button/Button";
 import Modal from "../../../components/funcComponents/Modal";
+/* redux */
+import { connect } from "react-redux";
+import { setWorkspaces } from "../../../redux/ducks/workspacesDuck";
+
+/* Services WorkspaceApi */
 
 const mapStateToProps = (state) => {
-	return { email: state.userMeDuck.user.email };
+	return {
+		email: state.userMeDuck.user.email,
+		workspaces: state.workspacesDuck.workspaces,
+	};
 };
 class Workspaces extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modalflag: false,
+		};
+	}
+	componentDidMount() {
+		this.props.dispatch(
+			setWorkspaces(getWorkSpacesByEmail(this.props.email))
+		);
+	}
+	componentDidUpdate() {}
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            modalflag: false,
-            workspacesByEmail: null
-        }
-    }
-    componentDidMount() {
-        this.setState(
-            {
-                workspacesByEmail: getWorkSpacesByEmail(this.props.email),
-            }
-        )
-    }
-    componentDidUpdate() {
-    }
+	/*adding New Work space */
+	addNewWorkspace = () => {
+		this.setState({
+			modalflag: true,
+		});
 
-    /*adding New Work space */
-    addNewWorkspace = () => {
-
-        this.setState({
-            modalflag: true,
-        })
-
-
-
-        let ob = {
-            name: 'ciccio',
-            users: [{ email: 'obama', role: 'sub' }]
-        }
-        addWorkSpace(ob)
-    }
-    render() {
-        return (
-            <div className="container-workspaces">
-                {
-                    this.state.modalflag &&
-                    <Modal />
-                }
-                <div className="aside-left">
-                    <div className="branch-aside-workspace">
-                        spazi di lavoro
-                        <Button label="+" onClick={this.addNewWorkspace} />
-                    </div>
-                    <div>
-
-                    </div>
-
-
-                </div>
-                <div className="aside-right">
-
-                </div>
-            </div>
-        )
-    }
-
+		let ob = {
+			name: "ciccio",
+			users: [{ email: this.props.email, role: "admin" }],
+		};
+		addWorkSpace(ob);
+		this.props.dispatch(
+			setWorkspaces(getWorkSpacesByEmail(this.props.email))
+		);
+	};
+	render() {
+		return (
+			<div className="container-workspaces">
+				{this.state.modalflag && <Modal />}
+				{this.props.workspaces.map((w, i) => (
+					<div key={i + "kuyyfvk"}>Workspace</div>
+				))}
+				<div className="aside-left">
+					<div className="branch-aside-workspace">
+						spazi di lavoro
+						<Button
+							label="+"
+							onClick={this.addNewWorkspace}
+						/>
+					</div>
+					<div></div>
+				</div>
+				<div className="aside-right"></div>
+			</div>
+		);
+	}
 }
 
 export default connect(mapStateToProps)(Workspaces);
